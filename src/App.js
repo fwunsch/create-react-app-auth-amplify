3c4058cd872b42b0bee4b0632e4070c8afa9dd33
Felix Wunsch <wunsch.felix@gmail.com>
2021-09-07 21:40:33 +0200

    add missing import

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import Amplify, { Auth } from 'aws-amplify';
 import aws_exports from './aws-exports';
 Amplify.configure(aws_exports);
 
+import Clock from './Clock.js'
+
 class App extends Component {
   render() {
     return (
