fbb401a16714fb6f9240564c08c326b4a695f418
Felix Wunsch <wunsch.felix@gmail.com>
2021-09-07 23:01:24 +0200

    update Clock.js

--- src/Clock.js
+++ src/Clock.js
@@ -3,7 +3,10 @@ import React from 'react'
 class Clock extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {date: new Date()};
+        this.state = {
+            date: new Date(),
+            accel: 0
+        };
     }
 
     componentDidMount() {
@@ -26,8 +29,13 @@ class Clock extends React.Component {
     render() {
         return (
             <div>
+                <div>
                 <h1>This is a ticking clock</h1>
                 <h2>Current time is {this.state.date.toLocaleDateString()} or simply {this.state.date.getTime()}.</h2>
+                </div>
+                <div>
+                    <h1>This is 3D acceleration: {this.state.accel} m/s</h1>
+                </div>
             </div>
         )
     }
