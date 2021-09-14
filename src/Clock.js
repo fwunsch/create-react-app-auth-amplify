ffdabcab80b693bae67bd6a8aea517d0f6746e6e
Felix Wunsch <wunsch.felix@gmail.com>
2021-09-14 23:42:18 +0200

    add dummy plotly plot

--- src/Clock.js
+++ src/Clock.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import Plot from 'react-plotly.js';
+
 
 class Clock extends React.Component {
     constructor(props) {
@@ -58,9 +60,19 @@ class Clock extends React.Component {
             <button onClick={this.handleClick}>
                 {this.state.isRunning ? 'ON' : 'OFF'}
             </button>
-            <p>X: {this.state.Xarr}</p>
-            <p>Y: {this.state.Yarr}</p>
-            <p>Z: {this.state.Zarr}</p>
+            <Plot
+                data={[
+                {
+                    x: [1, 2, 3],
+                    y: [2, 6, 3],
+                    type: 'scatter',
+                    mode: 'lines+markers',
+                    marker: {color: 'red'},
+                },
+                {type: 'bar', x: [1, 2, 3], y: [2, 5, 3]},
+                ]}
+                layout={ {width: 320, height: 240, title: 'A Fancy Plot'} }
+            />
             </div>
         )
     }
