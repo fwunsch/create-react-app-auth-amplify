329a2558edcf8d32934ea15868ed2332877cf755
Felix Wunsch <wunsch.felix@gmail.com>
2021-09-14 23:55:44 +0200

    try something

--- src/Clock.js
+++ src/Clock.js
@@ -63,15 +63,13 @@ class Clock extends React.Component {
             <Plot
                 data={[
                 {
-                    x: [1, 2, 3],
                     y: [2, 6, 3],
                     type: 'scatter',
                     mode: 'lines+markers',
                     marker: {color: 'red'},
-                },
-                {type: 'bar', x: [1, 2, 3], y: [2, 5, 3]},
+                }
                 ]}
-                layout={ {width: 320, height: 240, title: 'A Fancy Plot'} }
+                layout={ {width: 320, height: 240, title: 'Acceleration values'} }
             />
             </div>
         )
