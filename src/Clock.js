68c9513ed28b0a2bbc150a7e05404073825c298a
Felix Wunsch <wunsch.felix@gmail.com>
2021-09-16 00:11:47 +0200

    remove more code temporarily for debugging

--- src/Clock.js
+++ src/Clock.js
@@ -83,14 +83,14 @@ class Clock extends React.Component {
             layout={ {height: window.innerWidth, width: window.innerWidth, title: 'Acceleration values'} }
             />);
 
-            let posX = [0];
-            let posY = [0];
-            let posZ = [0];
-            for (let i=0; i<this.state.Xarr.length; i++){
-                posX.push(posX.at(-1) + this.state.Xarr(i));
-                posY.push(posY.at(-1) + this.state.Yarr(i));
-                posZ.push(posZ.at(-1) + this.state.Zarr(i));
-            }
+            // let posX = [0];
+            // let posY = [0];
+            // let posZ = [0];
+            // for (let i=0; i<this.state.Xarr.length; i++){
+            //     posX.push(posX.at(-1) + this.state.Xarr(i));
+            //     posY.push(posY.at(-1) + this.state.Yarr(i));
+            //     posZ.push(posZ.at(-1) + this.state.Zarr(i));
+            // }
             // posplotjsx = (<Plot
             //     data={[
             //     {
