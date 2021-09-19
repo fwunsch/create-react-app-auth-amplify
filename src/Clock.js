f4dcdcef46f4a9b37dd029ce466ed079db3e42ca
Felix Wunsch <wunsch.felix@gmail.com>
2021-09-19 22:08:50 +0200

    fix bug

--- src/Clock.js
+++ src/Clock.js
@@ -83,40 +83,40 @@ class Clock extends React.Component {
             layout={ {height: window.innerWidth, width: window.innerWidth, title: 'Acceleration values'} }
             />);
 
-            // let posX = [0];
-            // let posY = [0];
-            // let posZ = [0];
-            // for (let i=0; i<this.state.Xarr.length; i++){
-            //     posX.push(posX.at(-1) + this.state.Xarr(i));
-            //     posY.push(posY.at(-1) + this.state.Yarr(i));
-            //     posZ.push(posZ.at(-1) + this.state.Zarr(i));
-            // }
-            // posplotjsx = (<Plot
-            //     data={[
-            //     {
-            //         y: posX,
-            //         type: 'scatter',
-            //         mode: 'lines+markers',
-            //         marker: {color: 'red'},
-            //         name: 'X'
-            //     },
-            //     {
-            //         y: posY,
-            //         type: 'scatter',
-            //         mode: 'lines+markers',
-            //         marker: {color: 'blue'},
-            //         name: 'Y'
-            //     },
-            //     {
-            //         y: posZ,
-            //         type: 'scatter',
-            //         mode: 'lines+markers',
-            //         marker: {color: 'green'},
-            //         name: 'Z'
-            //     }
-            //     ]}
-            //     layout={ {height: window.innerWidth, width: window.innerWidth, title: 'Position'} }
-            //     />);
+            let posX = [0];
+            let posY = [0];
+            let posZ = [0];
+            for (let i=0; i<this.state.Xarr.length; i++){
+                posX.push(posX.at[-1] + this.state.Xarr[i]);
+                posY.push(posY.at[-1] + this.state.Yarr[i]);
+                posZ.push(posZ.at[-1] + this.state.Zarr[i]);
+            }
+            posplotjsx = (<Plot
+                data={[
+                {
+                    y: posX,
+                    type: 'scatter',
+                    mode: 'lines+markers',
+                    marker: {color: 'red'},
+                    name: 'X'
+                },
+                {
+                    y: posY,
+                    type: 'scatter',
+                    mode: 'lines+markers',
+                    marker: {color: 'blue'},
+                    name: 'Y'
+                },
+                {
+                    y: posZ,
+                    type: 'scatter',
+                    mode: 'lines+markers',
+                    marker: {color: 'green'},
+                    name: 'Z'
+                }
+                ]}
+                layout={ {height: window.innerWidth, width: window.innerWidth, title: 'Position'} }
+                />);
         }
         return (
             <div>
